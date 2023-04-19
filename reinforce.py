460f7bc843a003f5a13ac3418096cd09a213d185
Mike Xiangyu Liu <xl142@illinois.edu>
2023-04-18 21:19:50 -0500

    train on full continuous pendulum with sparse reward

--- reinforce.py
+++ reinforce.py
@@ -4,6 +4,8 @@ import torch.optim as optim
 from torch.distributions import Categorical
 import matplotlib.pyplot as plt
 import numpy as np
+from torch.utils.tensorboard import SummaryWriter
+import time
 
 
 class TabularPolicy(nn.Module):
@@ -19,6 +21,135 @@ class TabularPolicy(nn.Module):
 
     def load(self, file):
         self.weights = torch.load(file)
+
+
+class GaussianPolicy(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_dim):
+        super(GaussianPolicy, self).__init__()
+        self.fc1 = nn.Linear(state_dim, hidden_dim)
+        self.mu_head = nn.Linear(hidden_dim, action_dim)
+        self.sigma_head = nn.Linear(hidden_dim, action_dim)
+
+    def forward(self, state):
+        if not isinstance(state, torch.Tensor):
+            state = torch.tensor(state, dtype=torch.float32)
+        x = torch.tanh(self.fc1(state))
+        mu = torch.tanh(self.mu_head(x))
+        log_std = nn.functional.softplus(self.sigma_head(x))
+        log_std = torch.clamp(log_std, min=-20, max=2)
+        sigma = torch.exp(log_std)
+        return mu, sigma
+
+
+def calculate_gae(rewards, values, next_value, gamma, tau):
+    gae = 0
+    advantages = []
+    returns = []
+    for t in reversed(range(len(rewards))):
+        delta = rewards[t] + gamma * next_value - values[t]
+        gae = delta + gamma * tau * gae
+        advantages.insert(0, gae)
+        returns.insert(0, gae + values[t])
+        next_value = values[t]
+    return advantages, returns
+
+
+class ValueNetwork(nn.Module):
+    def __init__(self, state_dim, hidden_dim):
+        super(ValueNetwork, self).__init__()
+        self.fc1 = nn.Linear(state_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, 1)
+
+    def forward(self, state):
+        x = torch.relu(self.fc1(state))
+        value = self.fc2(x)
+        return value
+
+
+def train_gaussian_policy(policy, value_network, policy_optimizer, value_optimizer, env, episodes, gamma=0.99,
+                          tau=0.95):
+    returns_out = []
+    writer = SummaryWriter(log_dir='./logs/' + str(time.time()))
+    for episode in range(episodes):
+        state = env.reset()
+        log_probs, values, rewards, states = [], [], [], []
+
+        while True:
+            state_tensor = torch.tensor(state, dtype=torch.float32).unsqueeze(0)
+            mu, sigma = policy(state_tensor)
+            action_dist = torch.distributions.Normal(mu, sigma)
+            action = action_dist.sample()
+            next_state, reward, done = env.step(action.squeeze(0).detach().numpy().flatten()[0])
+
+            log_prob = action_dist.log_prob(action).sum(-1)
+            value = value_network(state_tensor)
+
+            log_probs.append(log_prob)
+            values.append(value.item())
+            rewards.append(reward)
+            states.append(state)
+
+            state = next_state
+
+            if done:
+                break
+
+        next_value = value_network(torch.tensor(next_state, dtype=torch.float32).unsqueeze(0)).item()
+        returns, advantages = calculate_gae(rewards, values, next_value, gamma, tau)
+        policy_loss = update_policy(policy, policy_optimizer, log_probs, advantages)
+        value_loss = update_value_network(value_network, value_optimizer, states, returns)
+
+        returns_out.append(sum(rewards))
+
+        # Logging to TensorBoard
+        writer.add_scalar('Total Reward', sum(rewards), episode)
+        writer.add_scalar('Policy Loss', policy_loss, episode)
+        writer.add_scalar('Value Loss', value_loss, episode)
+
+        print(
+            f"Episode {episode + 1}: Policy Loss = {policy_loss:.4f}, Value Loss = {value_loss:.4f}, Total Reward = {sum(rewards)}")
+
+        if (episode + 1) % 50 == 0:
+            torch.save(policy.state_dict(), f"policy_checkpoint_{episode + 1}.pth")
+            torch.save(value_network.state_dict(), f"value_checkpoint_{episode + 1}.pth")
+            print(f"Saved checkpoints at episode {episode + 1}")
+
+    return returns_out
+
+
+def calculate_returns(rewards, gamma):
+    returns = []
+    R = 0
+    for r in reversed(rewards):
+        R = r + gamma * R
+        returns.insert(0, R)
+    return returns
+
+
+def update_policy(policy, optimizer, log_probs, returns):
+    loss = 0
+    for log_prob, G_t in zip(log_probs, returns):
+        loss += -log_prob * G_t
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    return loss.item()
+
+
+def update_value_network(value_network, optimizer, states, returns):
+    states = torch.tensor(states, dtype=torch.float32)
+    returns = torch.tensor(returns, dtype=torch.float32).detach()
+
+    values = value_network(states)
+    loss = nn.MSELoss()(values.view(-1), returns)
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    return loss.item()
 
 
 def train_policy(policy, optimizer, env, episodes, gamma=0.99):
@@ -88,24 +219,4 @@ def plot_policy(policy, width, height, file):
     plt.savefig(file)
 
 
-def calculate_returns(rewards, gamma):
-    returns = []
-    R = 0
-    for r in reversed(rewards):
-        R = r + gamma * R
-        returns.insert(0, R)
-    return returns
 
-
-def update_policy(policy, optimizer, log_probs, returns):
-    loss = []
-    for log_prob, G_t in zip(log_probs, returns):
-        loss.append(-log_prob * G_t)
-
-    loss = torch.stack(loss).sum()
-
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-
-    return loss.item()
---
+++ test_pendulum.py
@@ -0,0 +1,103 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import pendulum
+from reinforce import *
+
+train = False
+
+
+def main():
+    # Create environment
+    env = pendulum.Pendulum()
+
+    # Initialize simulation
+    s = env.reset()
+
+    state_dim = 2
+    action_dim = 1
+
+    policy = GaussianPolicy(state_dim, action_dim, 64)
+    value_network = ValueNetwork(state_dim, 64)
+
+    returns = []
+    if train:
+        policy_optimizer = optim.Adam(policy.parameters(), lr=1e-3)
+        value_optimizer = optim.Adam(value_network.parameters(), lr=1e-3)
+
+        returns = train_gaussian_policy(policy, value_network, policy_optimizer, value_optimizer, env, episodes=50000,
+                                        gamma=0.99, tau=0.95)
+
+        # plot returns over episodes
+        plt.figure()
+        plt.plot(range(50000), returns)
+        plt.xlabel('Episode')
+        plt.ylabel('Total Reward')
+        plt.savefig('figures/test_gaussian_pendulum_returns.png')
+        plt.figure()
+    else:
+        policy.load_state_dict(torch.load("policy_checkpoint_50000.pth"))
+        value_network.load_state_dict(torch.load("value_checkpoint_50000.pth"))
+
+    ######################################
+    #
+    #   EXAMPLE OF CREATING A VIDEO
+    #
+
+    # Define a policy that maps every state to the "zero torque" action
+
+    # Simulate an episode and save the result as an animated gif
+    env.video(policy, filename='figures/test_discreteaction_pendulum.gif')
+
+    #
+    ######################################
+
+    ######################################
+    #
+    #   EXAMPLE OF CREATING A PLOT
+    #
+
+    # Initialize simulation
+    s = env.reset()
+
+    # Create dict to store data from simulation
+    data = {
+        't': [0],
+        's': [s],
+        'a': [],
+        'r': [],
+    }
+
+    # Simulate until episode is done
+    done = False
+    while not done:
+        a, _ = policy(s)
+        a = a.detach().numpy().flatten()[0]
+        (s, r, done) = env.step(a)
+        data['t'].append(data['t'][-1] + 1)
+        data['s'].append(s)
+        data['a'].append(a)
+        data['r'].append(r)
+
+    # Parse data from simulation
+    data['s'] = np.array(data['s'])
+    theta = data['s'][:, 0]
+    thetadot = data['s'][:, 1]
+    tau = [env._a_to_u(a) for a in data['a']]
+
+    # Plot data and save to png file
+    fig, ax = plt.subplots(3, 1, figsize=(10, 10))
+    ax[0].plot(data['t'], theta, label='theta')
+    ax[0].plot(data['t'], thetadot, label='thetadot')
+    ax[0].legend()
+    ax[1].plot(data['t'][:-1], tau, label='tau')
+    ax[1].legend()
+    ax[2].plot(data['t'][:-1], data['r'], label='r')
+    ax[2].legend()
+    ax[2].set_xlabel('time step')
+    plt.tight_layout()
+    plt.savefig('figures/test_discreteaction_pendulum.png')
+
+
+if __name__ == '__main__':
+    main()
